2134e5fec5aeb2b6473482ec40b237e23aa53cfd
priyanshu-rathore <priyanshurathore132@gmail.com>
2023-01-06 21:53:20 +0530

    UC4 insert value at specific position

--- DataStructures/LinkedList.cs
+++ DataStructures/LinkedList.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace DataStructures 
+namespace DataStructures
 {
     public class Node
     {
@@ -19,6 +19,8 @@ namespace DataStructures
         Node head;
         Node tail;
 
+        private int count;
+
         public void AddFirst(int value)// UC1 
         {
             Node newNode = new Node(value);
@@ -28,7 +30,9 @@ namespace DataStructures
             {
                 tail = head;
             }
-            System.Console.WriteLine(head.data+" is added");
+            System.Console.WriteLine(head.data + " is added");
+            count++;
+
         }
 
         public void AddLast(int data)//UC2
@@ -49,7 +53,48 @@ namespace DataStructures
                 temp.next = node;
             }
             System.Console.WriteLine(node.data + " is added");
+            count++;
         }
+        public void Insert(int value, int position)
+        {
+            // Check if position is valid
+            if (position < 0 || position > count)
+            {
+                return;
+            }
+
+            Node newNode = new Node(value);
+
+            // Insert at beginning
+            if (position == 0)
+            {
+                newNode.next = head;
+                head = newNode;
+                if (tail == null)
+                {
+                    tail = head;
+                }
+            }
+            // Insert at end
+            // else if (position == count)
+            // {
+            //     tail.next = newNode;
+            //     tail = newNode;
+            // }
+            // Insert in middle
+            else
+            {
+                Node current = head;
+                for (int i = 0; i < position - 1; i++)
+                {
+                    current = current.next;
+                }
+                newNode.next = current.next;
+                current.next = newNode;
+            }
+            count++;
+        }
+
 
         public void Display()
 
--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -5,8 +5,8 @@ namespace DataStructures{
         public static void Main(){
             linkedList list = new linkedList();
             list.AddLast(56);
-            list.AddLast(30);
             list.AddLast(70);
+            list.Insert(30,1);
             list.Display();
         }
     }
